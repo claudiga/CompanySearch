e2d450ebc7c569da37cc160aa21f15312346ae39
claude k <claudek@claudes-MBP.lan>
2024-05-15 07:32:51 +0100

    initial commit

---
+++ src/test/java/org/risknarrative/companysearch/CompanySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.risknarrative.companysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
